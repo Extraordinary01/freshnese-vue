c8125c25a9a16586dd39a05cd8a4a439cf1902ff
Extraordinary01 <begli.geld.08.12.01@gmail.com>
2021-12-17 12:30:22 +0500

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	publicDir: process.env.NODE_ENV === "production" ? "/freshnesecom" : "",
+	publicDir: process.env.NODE_ENV === "production" ? "/freshnese-vue" : "/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "/src"),
